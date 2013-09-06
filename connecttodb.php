5c84fc6c7f37fc39e9a1e5fa300974d3dfe9bb32
applecool <applecoolkmit@gmail.com>
2013-09-07 01:24:15 +0530

    changed connecttodb

--- connecttodb.php
+++ connecttodb.php
@@ -1,18 +1,18 @@
 <?php
 
-$conn_error = 'Couldn\'t connect to the database!'; 
+$conn_error = 'Couldn\'t connect to the database!';  //error message in case if there is a database connection error
 
-$mysql_host = 'localhost';
+$mysql_host = 'localhost'; 
 
 $mysql_user = 'root';
 
 $mysql_pass = '';
 
-$mysql_db = 'GRE';
+$mysql_db = 'GRE'; //database name 
 
 if(!@mysql_connect($mysql_host,$mysql_user, $mysql_pass) || !@mysql_select_db($mysql_db)){
 
-   die(mysql_error());
+   die(mysql_error()); 
    
 }
 ?>
