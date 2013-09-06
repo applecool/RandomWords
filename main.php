978785f1ba3551078153fff6c01cff0f083d9a38
applecool <applecoolkmit@gmail.com>
2013-09-07 01:27:15 +0530

    changed main.php

--- main.php
+++ main.php
@@ -1,10 +1,10 @@
 <?php
 
-require 'connecttodb.php'; 
+require 'connecttodb.php';  
 
 if(isset($_POST['word'])){
  //echo $_POST['word'];
-$word=$_POST['word'];
+$word=$_POST['word']; //grabbing the value typed in the text box and storing in the variable.
 
 if(!empty($word)){
  if(strlen($word)>55){
@@ -14,8 +14,10 @@ if(!empty($word)){
  	$query_run= mysql_query($query);
     $query_num_rows=mysql_num_rows($query_run);
     if($query_num_rows==1){
+    // check if the word already exists.
       echo 'Word '.$word.' already exists';
     }else{
+    // if not insert into the table.
      $insert_query="INSERT INTO `wordstable` VALUES(' ','".mysql_real_escape_string($word)."',1)";
      if($insert_queryrun=mysql_query($insert_query)){
       echo 'Word '.$word.' is added to the collection';
